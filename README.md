512997d119051f39805d35aa514fd1c06ecf129e
Nicholas Parson <Nparson88@gmail.com>
2021-05-10 19:14:41 -0400

    added comments and delete function

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# Note-taker-app
+ # Note-Taker-App
+# License: 
+![license](https://img.shields.io/badge/License-[Unlicensed](Unlicensed.txt)-blue?style=for-the-badge&logo=appveyor.svg)
+# Table of Contents 
+* [Description](#description)
+* [Usecase](#Usecase)
+* [Installation](#installation)
+* [Contributors](#contributors)
+* [License](#license)
+* [Questions](#questions)
+    
+## Description: 
+using express.js, save and delete notes stored on the local host
+## Installation: 
+express, fs, path
+## Usecase: 
+taking quick notes and saving them for later use
+## License: 
+[Unlicensed](Unlicensed.txt)
+## Contributors: 
+Nich Parson
+## Testing: 
+none
+## Questions: 
+Contact me by the following for any questions:
+* Github:(https://github.com/Nparson88)
+* Email: nparson88@gmail.com 
--- server.js
+++ server.js
@@ -1,28 +1,41 @@
+// required installs
 const express = require('express');
 const fs = require('fs')
 const path = require('path')
 const app = express();
-
+// heroku port
 const PORT = process.env.PORT || 3000;
+
 app.use("/assets", express.static("./assets"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 
-
+//routes to html pages and api
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, './index.html')));
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, './notes.html')));
 app.get('/api/notes', (req, res) => res.sendFile(path.join(__dirname, './db.json')));
-
+//post function to save notes
 app.post('/api/notes', (req, res) => {
     let newNote = req.body
-    let allNotes =  JSON.parse(fs.readFileSync("./db.json"));
-    let listLength = (allNotes.length).toString();
-    newNote.id = listLength
+    let allNotes = JSON.parse(fs.readFileSync("./db.json"));
+    let listLength = allNotes.length
+    newNote.id = listLength + 1
     allNotes.push(newNote)
 
     fs.writeFileSync('./db.json', JSON.stringify(allNotes))
     res.json(allNotes)
 })
+//delete function
+app.delete("/api/notes/:id", (req, res) => {
+    let allNotes = JSON.parse(fs.readFileSync("./db.json"));
+    let noteId = req.params.id
+    // after deleting note, filter out the rest to display
+    allNotes = allNotes.filter(data => {
+        return data.id != noteId;
+    })
+    fs.writeFileSync("./db.json", JSON.stringify(allNotes));
+    res.json(allNotes);
+});
 
 app.listen(PORT, () => console.log("Server listening on port " + PORT));
