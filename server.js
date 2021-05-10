d24d9e2d2b0f62b520d584030a7118cfb4114c86
Nicholas Parson <Nparson88@gmail.com>
2021-05-10 14:44:44 -0400

    app is taking in notes

--- server.js
+++ server.js
@@ -4,12 +4,25 @@ const path = require('path')
 const app = express();
 
 const PORT = process.env.PORT || 3000;
+app.use("/assets", express.static("./assets"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-const notes = [];
 
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
-app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'notes.html')));
-app.get('/api/notes', (req, res) => res.sendFile(path.join(__dirname, 'db.json')));
 
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, './index.html')));
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, './notes.html')));
+app.get('/api/notes', (req, res) => res.sendFile(path.join(__dirname, './db.json')));
+
+app.post('/api/notes', (req, res) => {
+    let newNote = req.body
+    let allNotes =  JSON.parse(fs.readFileSync("./db.json"));
+    let listLength = (allNotes.length).toString();
+    newNote.id = listLength
+    allNotes.push(newNote)
+
+    fs.writeFileSync('./db.json', JSON.stringify(allNotes))
+    res.json(allNotes)
+})
+
+app.listen(PORT, () => console.log("Server listening on port " + PORT));
